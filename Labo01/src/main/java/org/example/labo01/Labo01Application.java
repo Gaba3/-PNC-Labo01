bc5a0a3422a8d921c88b88ae94d953c38476b8e7
Gabriel <00103923@uca.edu.sv>
2026-04-22 14:25:50 -0600

    feat: Funcion para mostrar en consola

--- Labo01/src/main/java/org/example/labo01/Labo01Application.java
+++ Labo01/src/main/java/org/example/labo01/Labo01Application.java
@@ -1,7 +1,10 @@
 package org.example.labo01;
 
+import org.example.labo01.services.EspecimenService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Labo01Application {
@@ -10,4 +13,15 @@ public class Labo01Application {
 		SpringApplication.run(Labo01Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(EspecimenService especimenService){
+		return args -> {
+			System.out.println("=== INICIANDO APP ===");
+			especimenService.getAllEspecimen().forEach(e->
+					System.out.println("[S.T.A.R.S-REPORT] Nombre: " + e.getNombre() +
+							" | Nivel de Peligro: " + e.getNivelPeligro() +
+							" | Punto Debil: " + e.getPuntoDebil())
+			);
+		};
+	}
 }
